a52ae80b06e214081903067246704d92f63bebb4
viise <viise@outlook.com>
2020-04-18 19:31:48 +1000

    Improved psd

--- README.md
+++ README.md
@@ -1,46 +1,114 @@
 # psd
-Simple python script scheduler for one task.
+Simple python script scheduler.
 
 Usage
 -----
 - Linux:
-<pre>python psd.py 'your task here' 7 50 23 50 1 true</pre>
+<pre>python psd.py true</pre>
 - Windows (PowerShell):
-<pre>python.exe psd.py 'your task here' 7 50 23 50 1 false</pre>
+<pre>python.exe psd.py false</pre>
 - Windows (cmd):
-<pre>python.exe psd.py "your task here" 7 50 23 50 1 false</pre>
+<pre>python.exe psd.py false</pre>
 
 Parameters
 ----------
-psd.py <1> <2> <3> <4> <5> <6> <7>
-1) <1> - Your task, for example 'ping 8.8.8.8'
-2) <2> - Task start hour
-3) <3> - Task start minutes
-4) <4> - Task stop hour
-5) <5> - Task stop minutes
-6) <6> - After how many days need to repeat the task
-7) <7> - Through the shell or not [(what is it?)](https://docs.python.org/3/library/subprocess.html#frequently-used-arguments)
+psd.py <1>
+1) <1> - Through the shell or not [(what is it?)](https://docs.python.org/3/library/subprocess.html#frequently-used-arguments)
 
-In this way, this command <code>python psd.py 'ping 8.8.8.8' 7 50 23 50 1 true</code> means the following:</br>
-start command 'ping 8.8.8.8' today at 7:50 am, finish command today at 23:50, run again this task tommorow (today + 1 day) at the same time, through the shell. 
+psd.json
+--------
+This file contains all about jobs. Place in the same directory as psd.py.<br>
+File contents:
+<pre>
+{
+  "jobs": [
+    {
+      "name": "ping1",
+      "cmd": "ping 8.8.8.8",
+      "schedule": {
+        "start": {
+          "time": "18:39",
+          "day": 1
+        },
+        "finish": {
+          "time": "18:40"
+        }
+      }
+    },
+    {
+      "name": "ping2",
+      "schedule": {
+        "start": {
+          "time": "now"
+        },
+        "finish": {
+          "time": "never"
+        }
+      },
+      "cmd": "ping 8.8.8.8"
+    },
+    {
+      "name": "ls",
+      "cmd": "ls",
+      "schedule": {
+        "start": {
+          "time": "18:00",
+          "day": 1
+        },
+        "finish": {
+          "time": "18:50",
+          "day": 2
+        }
+      },
+      "repeat": {
+        "unit": "m",
+        "val": 1
+      }
+    }
+  ]
+}
+</pre>
+
+Fields description:<br>
+<code>*</code> - Field required.
+- <code>*"jobs"</code>: the list that contains task;
+    - <code>*"name"</code>: job name;
+    - <code>*"cmd"</code>: command for job;
+    - <code>*"schedule"</code>: contains information about job's start and finish:
+        - <code>*"start"</code>: contains information about job's start:
+            - <code>*"time"</code>: Task start time. Possible values:
+                1) Time in format <code>"hh:MM"</code>;
+                2) <code>"now"</code>: Start job now.
+            - <code>"day"</code>: - The number of days after which it is necessary to start the job, after it's 
+                                    completion. If <code>"time"</code> has value <code>"now"</code>, then this field is 
+                                    optional.
+        - <code>*"finish"</code>: contains information about job's finish:
+            - <code>*"time"</code>: Task finished time. Possible values:
+                1) Time in format <code>"hh:MM"</code>;
+                2) <code>"never"</code>: Never finished job.
+            - <code>"day"</code>: - The number of days indicating how many days the job must be completed.
+            If <code>"time"</code> has value <code>"never"</code>, then this field is optional.
+    - <code>"repeat"</code>: contains information about job's repeated when it's must be repeated throughout its 
+                             execution. It is suitable for jobs that are performed once:
+        - <code>"unit"</code> - Repeat time unit. Possible values:
+            1) <code>"s"</code> - seconds;
+            2) <code>"m"</code> - minutes;
+            3) <code>"h"</code> - hours.
+        - <code>"val"</code> - Repeat time value.
 
 Example
 -------
 <pre>
-$ python psd.py 'ping 8.8.8.8' 21 56 21 57 1 true   
+$ python psd.py true   
 [ Schedule started at 2020-04-09 21:56:55.908238 ]
-[ Job: ping 8.8.8.8 ]
-[ Start time: 21:56 ]
-[ Stop time:  21:57 ]
-[ Repeat every 1 days ]
-[ Through the shell: True ]
-[ Waiting for job starting... ]
-[ Job started at 2020-04-09 21:56:55.908286 ]
+[ Job 'ping' started at 2020-04-09 21:56:55.908286 ]
 PING 8.8.8.8 (8.8.8.8) 56(84) bytes of data.
 64 bytes from 8.8.8.8: icmp_seq=1 ttl=43 time=153 ms
 64 bytes from 8.8.8.8: icmp_seq=2 ttl=43 time=177 ms
 64 bytes from 8.8.8.8: icmp_seq=3 ttl=43 time=201 ms
 64 bytes from 8.8.8.8: icmp_seq=4 ttl=43 time=224 ms
 64 bytes from 8.8.8.8: icmp_seq=5 ttl=43 time=145 ms
-[ Job finished at + 2020-04-09 21:57:00.962440 ]
+[ Job 'ping' finished at + 2020-04-09 21:57:00.962440 ]
+[ Job 'ls' started at 2020-04-09 21:58:55.968286 ]
+LICENSE psd.json psd.py README.md
 </pre>
--- psd.py
+++ psd.py
@@ -1,49 +1,270 @@
+# Copyright (C) 2020  ViiSE
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import datetime
 import subprocess
-import sys
-import time
 import platform
-import os
-import signal
+import json
+import time
+import sys
 from datetime import timedelta
 
-global job
 
-job_prg = str(sys.argv[1])
-start_hour = int(sys.argv[2])
-start_minutes = int(sys.argv[3])
-stop_hour = int(sys.argv[4])
-stop_minutes = int(sys.argv[5])
-day_count = int(sys.argv[6])
-isShell = sys.argv[7].lower() == 'true'
+class Job:
+    def __init__(self, name, cmd, schedule, is_sh):
+        self.name = name
+        self.cmd = cmd
+        self.schedule = schedule
+        self.is_shell = is_sh
+        self.job = None
+        self.start_datetime = None
+        self.stop_datetime = None
 
-start_date_time = datetime.datetime.combine(datetime.date.today(), datetime.time(start_hour, start_minutes, 0))
-stop_date_time = datetime.datetime.combine(datetime.date.today(), datetime.time(stop_hour, stop_minutes, 0))
+    def try_start(self):  # True - job is running, False - job is not running
+        if self.schedule["start"]["time"] == "now":
+            if self.job is None:
+                self.job = subprocess.Popen(self.cmd, shell=self.is_shell)
+                print("[ Job '" + str(self.name) + "' started at " + str(datetime.datetime.now()) + " ]")
+            return True
+        else:
+            if self.job is None:
+                if self.start_datetime is None:
+                    start_h_m = int_time(self.schedule["start"]["time"])
+                    self.start_datetime = datetime.datetime.combine(
+                        datetime.date.today(),
+                        datetime.time(start_h_m[0], start_h_m[1], 0))
+
+                now = datetime.datetime.now()
+                if now > self.start_datetime:
+                    self.job = subprocess.Popen(self.cmd, shell=self.is_shell)
+                    print("[ Job '" + str(self.name) + "' started at " + str(now) + " ]")
+                    start_day = self.schedule["start"]["day"]
+                    if "day" in self.schedule["finish"]:
+                        start_day += self.schedule["finish"]["day"]
+
+                    start_h_m = int_time(self.schedule["start"]["time"])
+                    self.start_datetime = datetime.datetime.combine(
+                        datetime.date.today() + timedelta(days=start_day),
+                        datetime.time(start_h_m[0], start_h_m[1], 0))
+                    return True
+                else:
+                    return False
+            else:
+                return True
+
+    def try_stop(self):  # true - job is finished, false - job is not finished
+        if self.schedule["finish"]["time"] == "never":
+            return False
+        else:
+            if self.stop_datetime is None:
+                stop_day = 0
+                if "day" in self.schedule["finish"]:
+                    stop_day = self.schedule["finish"]["day"]
+
+                stop_h_m = int_time(self.schedule["finish"]["time"])
+                self.stop_datetime = datetime.datetime.combine(
+                    datetime.date.today() + timedelta(days=stop_day),
+                    datetime.time(stop_h_m[0], stop_h_m[1], 0))
+
+            now = datetime.datetime.now()
+            if now > self.stop_datetime:
+                if platform.system() == 'Windows':
+                    subprocess.call(['taskkill', '/F', '/T', '/PID', str(self.job.pid)])
+                else:
+                    self.job.kill()
+                self.job = None
+                print("[ Job '" + self.name + "' finished at + " + str(now) + " ]")
+
+                stop_day = self.schedule["start"]["day"]
+                if "day" in self.schedule["finish"]:
+                    stop_day += self.schedule["finish"]["day"]
+
+                stop_h_m = int_time(self.schedule["finish"]["time"])
+                self.stop_datetime = datetime.datetime.combine(
+                    datetime.date.today() + timedelta(days=stop_day),
+                    datetime.time(stop_h_m[0], stop_h_m[1], 0))
+
+
+class JobRep:
+    def __init__(self, name, cmd, schedule, is_sh, repeat):
+        self.name = name
+        self.cmd = cmd
+        self.schedule = schedule
+        self.is_shell = is_sh
+        self.start_datetime = None
+        self.stop_datetime = None
+        self.repeat = repeat
+        self.is_start = False
+        self.next_repeat = None
+
+    def try_start(self):  # True - job is running, False - job is not running
+        if self.is_start:
+            return True
+        elif self.schedule["start"]["time"] == "now":
+            self.is_start = True
+            print("[ Job '" + str(self.name) + "' started at " + str(datetime.datetime.now()) + " ]")
+            return True
+        else:
+            if self.start_datetime is None:
+                start_h_m = int_time(self.schedule["start"]["time"])
+                self.start_datetime = datetime.datetime.combine(
+                    datetime.date.today(),
+                    datetime.time(start_h_m[0], start_h_m[1], 0))
+
+            now = datetime.datetime.now()
+            if now > self.start_datetime:
+                subprocess.Popen(self.cmd, shell=self.is_shell)
+                print("[ Job '" + str(self.name) + "' started at " + str(now) + " ]")
+                start_day = self.schedule["start"]["day"]
+                if "day" in self.schedule["finish"]:
+                    start_day += self.schedule["finish"]["day"]
+
+                start_h_m = int_time(self.schedule["start"]["time"])
+                self.start_datetime = datetime.datetime.combine(
+                    datetime.date.today() + timedelta(days=start_day),
+                    datetime.time(start_h_m[0], start_h_m[1], 0))
+                self.is_start = True
+                self.next_repeat = calc_repeat(now, self.repeat)
+                return True
+            else:
+                return False
+
+    def try_stop(self):  # true - job is finished, false - job is not finished
+        if self.schedule["finish"]["time"] == "never":
+            return False
+        else:
+            if self.stop_datetime is None:
+                stop_day = 0
+                if "day" in self.schedule["finish"]:
+                    stop_day = self.schedule["finish"]["day"]
+
+                stop_h_m = int_time(self.schedule["finish"]["time"])
+                self.stop_datetime = datetime.datetime.combine(
+                    datetime.date.today() + timedelta(days=stop_day),
+                    datetime.time(stop_h_m[0], stop_h_m[1], 0))
+
+            now = datetime.datetime.now()
+            if now > self.stop_datetime:
+                stop_day = self.schedule["start"]["day"]
+                if "day" in self.schedule["finish"]:
+                    stop_day += self.schedule["finish"]["day"]
+
+                stop_h_m = int_time(self.schedule["finish"]["time"])
+                self.stop_datetime = datetime.datetime.combine(
+                    datetime.date.today() + timedelta(days=stop_day),
+                    datetime.time(stop_h_m[0], stop_h_m[1], 0))
+                self.is_start = False
+                print("[ Job '" + self.name + "' finished at + " + str(now) + " ]")
+                return True
+            else:
+                return False
+
+    def try_repeat(self):
+        now = datetime.datetime.now()
+        if now > self.next_repeat:
+            subprocess.Popen(self.cmd, shell=self.is_shell)
+            self.next_repeat = calc_repeat(now, self.repeat)
+
+
+def calc_repeat(dt, repeat):
+    if repeat["unit"] == "s":
+        return dt + timedelta(seconds=repeat["val"])
+    elif repeat["unit"] == "m":
+        return dt + timedelta(minutes=repeat["val"])
+    elif repeat["unit"] == "h":
+        return dt + timedelta(hours=repeat["val"])
+
+
+def int_time(t):
+    hour = int(t[0:2])
+    minute = int(t[3:5])
+    return [hour, minute]
+
+
+def is_time_format(t):
+    try:
+        time.strptime(t, '%H:%M')
+        return True
+    except ValueError:
+        return False
+
+
+is_shell = sys.argv[1].lower() == 'true'
+
+jobs = []
+jobs_r = []
+
+settings = json.load(open("psd.json"))
+for js in settings["jobs"]:
+    if "name" not in js:
+        print("Field 'name' is not found in job!")
+        exit(-1)
+    if "cmd" not in js:
+        print("Field 'cmd' is not found in job!")
+        exit(-1)
+    if "schedule" not in js:
+        print("Field 'schedule' is not found in job!")
+        exit(-1)
+    if "start" not in js["schedule"]:
+        print("Field 'start' is not found in job{schedule}!")
+        exit(-1)
+    if "time" not in js["schedule"]["start"]:
+        print("Field 'time' is not found in job{schedule{start}}!")
+        exit(-1)
+    if not is_time_format(js["schedule"]["start"]["time"]):
+        if js["schedule"]["start"]["time"] != "now":
+            print("Field 'time' has wrong pattern! Expected ##:##, actual " + str(js["schedule"]["start"]["time"]))
+            exit(-1)
+    if "finish" not in js["schedule"]:
+        print("Field 'finish' is not found in job{schedule}!")
+        exit(-1)
+    if "time" not in js["schedule"]["finish"]:
+        print("Field 'time' is not found in job{schedule{finish}}!")
+        exit(-1)
+    if not is_time_format(js["schedule"]["finish"]["time"]):
+        if js["schedule"]["finish"]["time"] != "never":
+            print("Field 'time' has wrong pattern! Expected ##:##, actual " + str(js["schedule"]["finish"]["time"]))
+            exit(-1)
+    if "repeat" in js:
+        if "unit" not in js["repeat"]:
+            print("Field 'unit' is not found in job{repeat}!")
+            exit(-1)
+        if "val" not in js["repeat"]:
+            print("Field 'val' is not found in job{repeat}!")
+            exit(-1)
+
+        jobs_r.append(JobRep(
+            js["name"],
+            js["cmd"],
+            js["schedule"],
+            is_shell,
+            js["repeat"]))
+    else:
+        jobs.append(Job(
+            js["name"],
+            js["cmd"],
+            js["schedule"],
+            is_shell))
 
 print("[ Schedule started at " + str(datetime.datetime.now()) + " ]")
-print("[ Job: " + job_prg + " ]")
-print("[ Start time: " + str(start_hour) + ":" + str(start_minutes) + " ]")
-print("[ Stop time:  " + str(stop_hour) + ":" + str(stop_minutes) + " ]")
-print("[ Repeat every " + str(day_count) + " days ]")
-print("[ Through the shell: " + str(isShell) + " ]")
-print("[ Waiting for job starting... ]")
-
 while True:
-    now = datetime.datetime.now()
-    if now > start_date_time:
-        job = subprocess.Popen(job_prg, shell=isShell)
-        print("[ Job started at " + str(now) + " ]")
-        # Calculate new start date
-        next_start_day_job = datetime.date.today() + timedelta(days=day_count)
-        start_date_time = datetime.datetime.combine(next_start_day_job, datetime.time(start_hour, start_minutes, 0))
-
-    if now > stop_date_time:
-        if platform.system() == 'Windows':
-            subprocess.call(['taskkill', '/F', '/T', '/PID',  str(job.pid)])
-        else:
-            job.kill()
-        print("[ Job finished at + " + str(now) + " ]")
-        # Calculate new stop date
-        next_stop_day_job = datetime.date.today() + timedelta(days=day_count)
-        stop_date_time = datetime.datetime.combine(next_stop_day_job, datetime.time(stop_hour, stop_minutes, 0))
+    for j in jobs:
+        if j.try_start():
+            j.try_stop()
+    for jr in jobs_r:
+        if jr.try_start():
+            if not jr.try_stop():
+                jr.try_repeat()
     time.sleep(1)
